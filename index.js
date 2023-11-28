335eaa3c53c264a2fc66408d7e3abdd2ab5e4806
MateuszMiku <mateuszmiku.mm@gmail.com>
2023-11-28 22:27:32 +0100

    populate display when pressing numbers

--- index.js
+++ index.js
@@ -15,5 +15,30 @@ const multiply = function(x, y) {
 };
 
 
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, operator, secondNumber) {
+  if (operator == "+") {
+     return sum(firstNumber, secondNumber)
+  } else if (operator == "-") {
+     return subtract(firstNumber, secondNumber)
+  } else if (operator == "/") {
+     return divide(firstNumber, secondNumber)
+  } else if (operator == "*") {
+     return multiply(firstNumber, secondNumber)
+  }
+}
+
+let numbers = Array.from(document.getElementsByClassName('number'));
+let display = document.querySelector('.display');
+
+
+numbers.forEach(num => num.addEventListener('click', function() {
+    display.innerHTML += this.innerHTML;
+    let displayValue = parseInt(display.innerHTML);
+    console.log(displayValue)
+}))
 
 
