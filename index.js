99f5d13bd3b4eb1df07d14f1124258c84c3c2ec9
MateuszMiku <mateuszmiku.mm@gmail.com>
2023-11-29 00:25:29 +0100

    add deleting numbers and clearing display

--- index.js
+++ index.js
@@ -34,11 +34,36 @@ function operate(firstNumber, operator, secondNumber) {
 let numbers = Array.from(document.getElementsByClassName('number'));
 let display = document.querySelector('.display');
 
-
+//displaying and storing numbers
 numbers.forEach(num => num.addEventListener('click', function() {
     display.innerHTML += this.innerHTML;
     let displayValue = parseInt(display.innerHTML);
-    console.log(displayValue)
-}))
+    console.log(displayValue);
+}));
+
+// Clear button
+const clearButton = document.getElementById('clear');
+    clearButton.addEventListener('click', function() {
+    display.innerHTML = null;
+});
+
+// Delete button
+const deleteButton = document.getElementById('delete');
+
+deleteButton.addEventListener('click', function(){
+
+  let displayValue
+
+    if (display.innerHTML.length > 1 && display.innerHTML !== 0) {
+        displayValue = parseInt(display.innerHTML.slice(0, -1));
+        display.innerHTML = displayValue;
+        console.log(displayValue);
+    } else {
+        displayValue = 0;
+        display.innerHTML = null;
+        console.log(displayValue);
+    }
+});
+
 
 
